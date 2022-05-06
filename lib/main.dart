896864e88fcb9104d43147dfbc27c9f4d79a8988
LAPTOP-EPT4T92E\masar <masarova.g@gmail.com>
2022-05-06 10:58:58 +0200

    test palec

--- lib/main.dart
+++ lib/main.dart
@@ -113,7 +113,7 @@ class _MyHomePageState extends State<MyHomePage> {
                           mainValue = mainValue + "";
                         });
                       },
-                      value: "",
+                      value: "👍",
                       hasLowerOpacity: true),
                 ],
               ),
